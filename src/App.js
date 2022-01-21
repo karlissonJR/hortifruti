502d7de1f06dd12685d107eee4d8ce67b915334d
karlissonJR <karlissonj@gmail.com>
2022-01-21 11:21:24 -0300

    correção do erro de cors na chamada da api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
+import fruityviceApi from './api/fruityvice'
+
 function App() {
+  
+  const [fruits, setFruits] = useState([])
+
+  useEffect(() => {
+    fruityviceApi.get('/all').then(response => {
+      setFruits(response.data)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <ul>
+          {fruits.map(fruit => {
+            return (
+              <li key={fruit.id}>
+                {fruit.name}
+              </li>
+            )
+          })}
+        </ul>
+        
       </header>
     </div>
   );
--- src/api/fruityvice.js
+++ src/api/fruityvice.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
-const fruityvice = axios.create({
-    baseURL: 'https://www.fruityvice.com/api/fruit/'
+const fruityviceApi = axios.create({
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit',
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    }
 });
+
+export default fruityviceApi
